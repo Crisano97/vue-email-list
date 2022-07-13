4be9189942892dbc6e2250f5cf3bd23487cf9ac5
Crisano97 <97keb6@gmail.com>
2022-07-13 16:53:36 +0200

    emails list showed after checking array's length

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const app = new Vue(
         el: '#app',
 
         data: {
-            
+            //mi creo un array che andrà a contenere tutte le email generate;
             emails: [
 
             ]
@@ -14,13 +14,13 @@ const app = new Vue(
                 console.log(args);
             },
             numberOfRandomEmail: function () {
-                
+                //uso un ciclo for per fare richiesta all'API per 10 volte;
                 for (let i = 0; i <= 9; i++) {
                     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                         .then((response) => {
                             console.log(response.data.response)
+                            //pusho le email generate, all'interno del mio array;
                             this.emails.push(response.data.response);
-                            
                         })
                 }
             }
